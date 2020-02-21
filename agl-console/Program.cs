d96771c5b2c3148e8ef871d29e7ed7031242f829
CECILIA COOK <101938034@student.swin.edu.au>
2020-02-22 00:40:23 +1100

    Analog name printing - no sort yet

--- agl-console/Program.cs
+++ agl-console/Program.cs
@@ -2,13 +2,16 @@
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Text.Json;
-using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace agl_console
 {
     class Program
     {
-         
+
+
+
         private static async  Task<string> ProcessJson()
         {
             var stringTask = client.GetStringAsync("http://agl-developer-test.azurewebsites.net/people.json");
@@ -26,13 +29,30 @@ namespace agl_console
 
         static async Task Main(string[] args)
         {
+
+            List<Pet> Cats = new List<Pet>();
+
             var processedMsg = await ProcessJson();
             var people = JsonSerializer.Deserialize<Person[]>(processedMsg);
 
 
             foreach(Person person in people)
             {
+                if (person.Pets != null)
+                {
+                    foreach (Pet pet in person.Pets)
+                    {
+                        if (pet != null && pet.Type == "Cat")
+                        {
+                        Console.WriteLine(pet.Name);
+
+                        }
+
+                    }
+                }
+              
                 Console.WriteLine(person);
+                   
             }
 
         }
